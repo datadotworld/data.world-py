670f1af275741e4f2b7049189ea697c7c0926400
bryon <bryon@data.world>
2017-01-19 16:31:16 -0600

    it's 2017

--- datadotworld/__init__.py
+++ datadotworld/__init__.py
@@ -1,6 +1,6 @@
 '''
 data.world-py
-Copyright 2016 data.world, Inc.
+Copyright 2017 data.world, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the
