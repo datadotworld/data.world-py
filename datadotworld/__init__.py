a813f9ff488ed4a85b535a2d5f48acca74f0db3c
Rafael Pereira <rflprr@users.noreply.github.com>
2017-06-01 16:23:06 -0500

    Fix issue with requires_extra in PyPI (#45)
    * Fix issue with requires_extra in PyPI
    * Bump version

--- datadotworld/__init__.py
+++ datadotworld/__init__.py
@@ -30,7 +30,7 @@ import weakref
 from datadotworld.config import FileConfig, ChainedConfig
 from datadotworld.datadotworld import DataDotWorld
 
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 
 # Convenience top-level functions
 
