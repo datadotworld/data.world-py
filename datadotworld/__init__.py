20f5fbb2b3c8dbf9b9f20bff8499c00a4c84fb98
Rafael Pereira <rflprr@users.noreply.github.com>
2017-06-02 10:18:30 -0500

    Disable binary distribution

--- datadotworld/__init__.py
+++ datadotworld/__init__.py
@@ -30,7 +30,7 @@ import weakref
 from datadotworld.config import FileConfig, ChainedConfig
 from datadotworld.datadotworld import DataDotWorld
 
-__version__ = '1.2.3'
+__version__ = '1.2.4'
 
 # Convenience top-level functions
 
