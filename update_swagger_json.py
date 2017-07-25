d4a981f0ff1a37936fdcc768af83871e89227019
Tim Clemans <tim@hardworkingcoder.com>
2017-07-25 15:10:20 +0000

    add update_swagger_json.py

---
+++ update_swagger_json.py
@@ -0,0 +1,6 @@
+import requests
+api_swagger = requests.get('https://api.data.world/v0/swagger.json').text
+api_swagger = api_swagger.encode('ascii', 'ignore').decode('ascii')
+api_swagger = json.loads(api_swagger)
+with open('datadotworld/client/swagger-dwapi-def.json', 'w') as f:
+    f.write(json.dumps(api_swagger, indent=4, sort_keys=True))
