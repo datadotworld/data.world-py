85072d8f450fb32274e813e694a90dcc71418458
andela-gokerayi <gbolahan.okerayi@andela.com>
2018-02-05 18:42:06 +0100

    Swagger generated code

--- datadotworld/client/_swagger/apis/__init__.py
+++ datadotworld/client/_swagger/apis/__init__.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import
 # import apis into api package
 from .datasets_api import DatasetsApi
 from .download_api import DownloadApi
+from .insights_api import InsightsApi
+from .projects_api import ProjectsApi
 from .sparql_api import SparqlApi
 from .sql_api import SqlApi
 from .streams_api import StreamsApi
