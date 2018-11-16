42586f75108e21828cf4579a8c9491367c6391e7
Rafael Pereira <rflprr@users.noreply.github.com>
2018-11-16 14:52:13 -0600

    Fix py36 coverage error

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(
         'pytest-runner>=2.11,<3.0a',
     ],
     tests_require=[
-        'coverage>=4.4.2',
+        'coverage>=4.4.2,<=4.5.1',
         'doublex>=1.8.4,<2.0a',
         'pyhamcrest>=1.9.0,<2.0a',
         'responses>=0.5.1,<1.0a',
