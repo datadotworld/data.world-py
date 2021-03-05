e44ecab4a3b3ad65bb52e2a446a25b062a8c1bc1
Danial Betres <danialbetres@Danials-MacBook-Pro.local>
2021-03-05 13:21:18 -0500

    explore swagger codegen

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ test-report:
 	coverage report -m
 update_swagger_codegen:
 	pushd datadotworld/client; \
-	curl https://api.data.world/v0/swagger.json -o swagger-dwapi-def.json; \
+	curl https://raw.githubusercontent.com/datadotworld/dwapi-spec/master/src/main/resources/world/data/api/swagger.json -o swagger-dwapi-def.json; \
 	swagger-codegen generate -l python --git-repo-id=data.world-py --git-user-id=datadotworld \
 		--http-user-agent="data.world-py" --invoker-package=client \
 		-i swagger-dwapi-def.json -c swagger-codegen-config.json --release-note=""; \
---
+++ datadotworld/client/_swagger/api/__init__.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from _swagger.api.do_is_api import DOIsApi
+from _swagger.api.connections_api import ConnectionsApi
+from _swagger.api.datasets_api import DatasetsApi
+from _swagger.api.files_api import FilesApi
+from _swagger.api.insights_api import InsightsApi
+from _swagger.api.metadataanalysis_api import MetadataanalysisApi
+from _swagger.api.metadatacollections_api import MetadatacollectionsApi
+from _swagger.api.metadatadata_api import MetadatadataApi
+from _swagger.api.metadataglossary_api import MetadataglossaryApi
+from _swagger.api.metadatarelationships_api import MetadatarelationshipsApi
+from _swagger.api.projects_api import ProjectsApi
+from _swagger.api.queries_api import QueriesApi
+from _swagger.api.search_api import SearchApi
+from _swagger.api.streams_api import StreamsApi
+from _swagger.api.tables_api import TablesApi
+from _swagger.api.user_api import UserApi
+from _swagger.api.users_api import UsersApi
+from _swagger.api.webhooks_api import WebhooksApi
