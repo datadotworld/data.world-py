a0e41733c1029ee713b78383aadf23382c06b0ae
nikhilhegde-ddw <96458241+nikhilhegde-ddw@users.noreply.github.com>
2022-03-11 15:15:53 +0530

    Fix to install the python sdk with pandas for python version > 3.7 (#128)
    * Fix to install the python sdk with pandas for python version >= 3.7
    
    * Updated circle ci to use pyenv-tox:1.0.0 docker image
    
    * Update circle ci config to setup pyenv 3.7.12 3.8.12 3.9.10 3.10.2
    
    * Updated classifiers

--- datadotworld/__init__.py
+++ datadotworld/__init__.py
@@ -35,7 +35,7 @@ from datadotworld.config import (
 )
 from datadotworld.datadotworld import DataDotWorld, UriParam  # noqa: F401
 
-__version__ = '1.8.0'
+__version__ = '1.8.1'
 
 # Convenience top-level functions
 
