9cf755f0c93d47507356fd2cae40bb7f7351eb4e
neilnautiyal-ddw <104857504+neilnautiyal-ddw@users.noreply.github.com>
2023-04-19 12:04:44 -0400

    updating pypi version (#143)
    Co-authored-by: Neil Nautiyal <neilnautiyal@neils-mbp-3.mynetworksettings.com>

--- datadotworld/__init__.py
+++ datadotworld/__init__.py
@@ -35,7 +35,7 @@ from datadotworld.config import (
 )
 from datadotworld.datadotworld import DataDotWorld, UriParam  # noqa: F401
 
-__version__ = '1.8.3'
+__version__ = '1.8.5'
 
 # Convenience top-level functions
 
