fbc6be12fd7f7498b8b54897abfc54f3736f01a7
Yurii <yurii.feduniak@servicenow.com>
2026-09-11 11:56:21 +0300

    Bump version to 2.0.1

--- datadotworld/__init__.py
+++ datadotworld/__init__.py
@@ -35,7 +35,7 @@ from datadotworld.config import (
 )
 from datadotworld.datadotworld import DataDotWorld, UriParam  # noqa: F401
 
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 
 # Convenience top-level functions
 
